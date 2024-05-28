b7064e4a68b5d7ae960a1400e45a69cbd8b7524e
Brant Ashe <brantashe@gmail.com>
2024-05-28 14:22:23 -0500

    minimal test

--- src/app/(test)/test/layout.tsx
+++ src/app/(test)/test/layout.tsx
@@ -12,8 +12,24 @@ export const metadata: Metadata = {
 export default function RootLayout() {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <BgBanner />
+      <body
+        className={inter.className}
+        style={{
+          display: 'flex',
+          margin: 0,
+          alignItems: 'center',
+          height: '100dvh',
+        }}
+      >
+        <div
+          style={{
+            height: 400,
+            width: '100%',
+            background: 'red',
+          }}
+        >
+          test
+        </div>
       </body>
     </html>
   );
