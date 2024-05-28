83f27f3da8b8f8a76421364845843feb6a9da9d0
Brant Ashe <brantashe@gmail.com>
2024-05-28 02:11:24 -0500

    attempt orientation animation

--- src/components/BannerBg.tsx
+++ src/components/BannerBg.tsx
@@ -1,7 +1,28 @@
 'use client';
 
+import { useCallback } from 'react';
+
 import styles from './components.module.css';
 
 export default function BannerBg() {
-  return <div className={styles['banner-bg']} />;
+  const callbackRef = useCallback((node: HTMLDivElement) => {
+    // TODO: debounce
+    const handleOrientation = (event: DeviceOrientationEvent) => {
+      let x = event.beta; // -180 - 180
+      let y = event.gamma; // -90 - 90
+
+      if (node && x !== null && y !== null) {
+        node.style.backgroundPositionX = `${x * (100 / 180)}%`;
+        node.style.backgroundPositionY = `${y * (50 / 90)}%`;
+      }
+    };
+
+    window.addEventListener('deviceorientation', handleOrientation);
+
+    return () => {
+      window.addEventListener('deviceorientation', handleOrientation);
+    };
+  }, []);
+
+  return <div ref={callbackRef} className={styles['banner-bg']} />;
 }
