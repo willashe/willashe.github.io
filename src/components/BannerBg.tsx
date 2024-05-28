bc41e662902a1adf22d27ea60ce4e70b8ba9bf6b
Brant Ashe <brantashe@gmail.com>
2024-05-28 01:51:54 -0500

    fix build

--- src/components/BannerBg.tsx
+++ src/components/BannerBg.tsx
@@ -1,32 +1,7 @@
 'use client';
 
-import { useCallback, useEffect, useState } from 'react';
-
 import styles from './components.module.css';
 
 export default function BannerBg() {
-  const [orientation, setOrientation] = useState({
-    beta: 0,
-    gamma: 0,
-  });
-
-  const handleOrientation = useCallback((event: DeviceOrientationEvent) => {
-    let x = event.beta; // In degree in the range [-180,180)
-    let y = event.gamma; // In degree in the range [-90,90)
-
-    setOrientation({
-      beta: x || 0,
-      gamma: y || 0,
-    });
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('deviceorientation', handleOrientation);
-
-    return () => {
-      window.addEventListener('deviceorientation', handleOrientation);
-    };
-  }, [handleOrientation]);
-
   return <div className={styles['banner-bg']} />;
 }
