d78cd8d4ce68ef431dd2be0008ed3b735c79a2ba
Brant Ashe <brantashe@gmail.com>
2024-05-28 03:27:29 -0500

    test

--- src/components/BannerBg.tsx
+++ src/components/BannerBg.tsx
@@ -13,13 +13,10 @@ export default function BannerBg() {
   useEffect(() => {
     // TODO: debounce
     const handleOrientation = (event: DeviceOrientationEvent) => {
-      let x = Math.round(event.beta || 0); // -180 - 180
-      let y = Math.round(event.gamma || 0); // -90 - 90
+      let x = event.beta || 0; // -180 - 180
+      let y = event.gamma || 0; // -90 - 90
 
-      setOrientation({
-        x,
-        y,
-      });
+      setOrientation({ x, y });
     };
 
     window.addEventListener('deviceorientation', handleOrientation);
@@ -36,7 +33,7 @@ export default function BannerBg() {
         style={{
           animation: orientation ? 'none' : undefined,
           backgroundPositionX: orientation
-            ? `${(orientation.x + 180) * (100 / 360)}%`
+            ? `${Math.round((orientation.x + 180) * (100 / 360))}%`
             : undefined,
         }}
       />
