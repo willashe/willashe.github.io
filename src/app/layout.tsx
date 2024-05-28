fffc91c3ee412b38c4f3c77a517275d1d07e1b78
Brant Ashe <brantashe@gmail.com>
2024-05-28 04:29:23 -0500

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import BannerBg from '@/components/BannerBg';
 import type { Metadata } from 'next';
 import { Assistant } from 'next/font/google';
-import { ReactNode } from 'react';
 
 import './globals.css';
 
@@ -12,17 +11,13 @@ export const metadata: Metadata = {
   description: 'Will Ashe - Software Engineer - Austin, TX',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
+export default function RootLayout() {
   return (
     <html lang="en">
       <body className={inter.className}>
         <main className="main">
           <BannerBg />
-          <div className="app">{children}</div>
+          <div className="app">test</div>
         </main>
       </body>
     </html>
