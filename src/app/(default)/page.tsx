7e82d83b900b2931c267ef20a06a6c36ba2d8cae
Brant Ashe <brantashe@gmail.com>
2024-05-28 19:53:37 -0500

    debug

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -3,14 +3,30 @@
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Skillset from '@/components/Skillset';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import styles from './page.module.css';
 
 export default function Home() {
+  const [height, setHeight] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+
   // TODO: revisit this...hack to fix android chrome bug that has problems with svh on fresh tab page load
   const callbackRef = useCallback((node: HTMLDivElement) => {
-    node.style.height = `${window.innerHeight}px`;
+    const handleResize = () => {
+      node.style.height = `${window.innerHeight}px`;
+
+      const rect = node.getBoundingClientRect();
+      setHeight(rect.height);
+      setWindowHeight(window.innerHeight);
+    };
+
+    addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      removeEventListener('resize', handleResize);
+    };
   }, []);
 
   return (
@@ -19,6 +35,21 @@ export default function Home() {
       <div className={styles['right-container']}>
         <Skillset />
         <Projects />
+      </div>
+
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          margin: 10,
+          fontFamily: 'monospace',
+          color: 'white',
+          fontSize: 8,
+        }}
+      >
+        <div>Container Height: {height}</div>
+        <div>Window Height: {windowHeight}</div>
       </div>
     </div>
   );
