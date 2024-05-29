49f89bf5750af51ff3de0d43dcba49e65e922f23
Brant Ashe <brantashe@gmail.com>
2024-05-28 19:25:38 -0500

    revert

---
+++ src/app/(moretest)/moretest/layout.tsx
@@ -0,0 +1,28 @@
+import BgBanner from '@/components/BgBanner';
+import type { Metadata } from 'next';
+import { Assistant } from 'next/font/google';
+import { ReactNode } from 'react';
+
+import '../../globals.scss';
+
+const inter = Assistant({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Will Ashe',
+  description: 'Will Ashe - Software Engineer - Austin, TX',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <BgBanner />
+        <main className="main">{children}</main>
+      </body>
+    </html>
+  );
+}
